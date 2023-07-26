0a31e0903a0981a4a5dba0b875153b439ee69e5d
CesarioStoquero <140554286+CesarioStoquero@users.noreply.github.com>
2023-07-26 17:22:15 -0300

    Add files via upload

---
+++ Csharp_ConsumindoApi_+Json/Program.cs
@@ -0,0 +1,34 @@
+﻿using ScreenSound_04.Modelos;
+using System.Text.Json;
+using ScreenSound_04.Filtros;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+
+        LinqFilter.FiltrarMusicasEmCSharp(musicas);
+       // musicas[1].ExibirDetalhesDaMusica();
+       // LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+       // LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
+       // LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "rock");
+       // LinqFilter.FiltrarMusicasDeUmArtista(musicas, "U2");
+
+        var musicasPreferidasdoCesario = new MusicasPreferidas("Cesario");
+        musicasPreferidasdoCesario.AdicionarMusicasFavoritas(musicas[1]);
+        musicasPreferidasdoCesario.AdicionarMusicasFavoritas(musicas[377]);
+        musicasPreferidasdoCesario.AdicionarMusicasFavoritas(musicas[4]);
+        musicasPreferidasdoCesario.AdicionarMusicasFavoritas(musicas[6]);
+        musicasPreferidasdoCesario.AdicionarMusicasFavoritas(musicas[1467]);
+
+        musicasPreferidasdoCesario.ExibirMusicasFavoritas();
+
+        musicasPreferidasdoCesario.GerarArquivoJson();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+}
